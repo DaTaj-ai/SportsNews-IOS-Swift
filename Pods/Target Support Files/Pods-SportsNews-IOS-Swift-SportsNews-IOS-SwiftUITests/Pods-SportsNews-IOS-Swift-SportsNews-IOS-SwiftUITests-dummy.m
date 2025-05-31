4f0e77ef3e80fbaeb22c81bfb0abf4e14423706e
Dataj <mohamedtajeldinai@gmail.com>
2025-05-31 06:46:20 -0700

    update .gitignore

---
+++ Pods/Target Support Files/Pods-SportsNews-IOS-Swift-SportsNews-IOS-SwiftUITests/Pods-SportsNews-IOS-Swift-SportsNews-IOS-SwiftUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SportsNews_IOS_Swift_SportsNews_IOS_SwiftUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SportsNews_IOS_Swift_SportsNews_IOS_SwiftUITests
+@end
---
+++ Pods/Target Support Files/Pods-SportsNews-IOS-SwiftTests/Pods-SportsNews-IOS-SwiftTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SportsNews_IOS_SwiftTests : NSObject
+@end
+@implementation PodsDummy_Pods_SportsNews_IOS_SwiftTests
+@end
